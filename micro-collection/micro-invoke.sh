5de9a1dcf376e2452c3c74e78d6158bbed3b8f29
arearnest7 <arearnest7@earnest-vm-1.engr.tamu.edu>
2024-03-07 17:27:04 -0600

    modified k8s/k3s deployments to latest format

--- micro-collection/micro-invoke.sh
+++ micro-collection/micro-invoke.sh
@@ -4,39 +4,39 @@ SIZE=${2:-1000000}
 sed -i "s/1000000/$SIZE/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a/func.yaml
 sed -i "s/1000000/$SIZE/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-b/func.yaml
 sed -i "s/1000000/$SIZE/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a-b/func.yaml
-sed -i "s/1000000/$SIZE/g" micro-rpc-grpc.yaml
-sed -i "s/1000000/$SIZE/g" micro-rpc-pod.yaml
-sed -i "s/1000000/$SIZE/g" micro-rpc-faastroute.yaml
+sed -i "s/1000000/$SIZE/g" micro-rpc-multi-pod.yaml
+sed -i "s/1000000/$SIZE/g" micro-rpc-single-pod.yaml
+sed -i "s/1000000/$SIZE/g" micro-rpc-single-mmap.yaml
 kn func deploy --build=false --push=false --path ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a
 kn func deploy --build=false --push=false --path ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-b
 sleep 1000s
-hey -n 10000 -c 1 -t 30 -o csv http://micro-rpc-a.default.10.125.189.107.sslip.io >> micro-rpc-gp-$DEST.csv
+hey -n 10000 -c 1 -t 30 -o csv http://micro-rpc-a.default.10.125.189.107.sslip.io >> micro-rpc-kn-original-$DEST.csv
 kn func delete micro-rpc-a
 kn func delete micro-rpc-b
 sleep 1000s
 kn func deploy --build=false --push=false --path ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a-b
 sleep 1000s
-hey -n 10000 -c 1 -t 30 -o csv http://micro-rpc-a-b.default.10.125.189.107.sslip.io >> micro-rpc-lp-$DEST.csv
+hey -n 10000 -c 1 -t 30 -o csv http://micro-rpc-a-b.default.10.125.189.107.sslip.io >> micro-rpc-kn-full-$DEST.csv
 kn func delete micro-rpc-a-b
 sleep 1000s
-kubectl apply -f micro-rpc-grpc.yaml
+kubectl apply -f micro-rpc-multi-pod.yaml
 sleep 1000s
-hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-grpc-$DEST.csv
-kubectl delete -f micro-rpc-grpc.yaml
+hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-multi-pod-$DEST.csv
+kubectl delete -f micro-rpc-multi-pod.yaml
 sleep 1000s
-kubectl apply -f micro-rpc-pod.yaml
+kubectl apply -f micro-rpc-single-pod.yaml
 sleep 1000s
-hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-pod-$DEST.csv
-kubectl delete -f micro-rpc-pod.yaml
+hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-single-pod-$DEST.csv
+kubectl delete -f micro-rpc-single-pod.yaml
 sleep 1000s
-kubectl apply -f micro-rpc-faastroute.yaml
+kubectl apply -f micro-rpc-single-mmap.yaml
 sleep 1000s
-hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-faastroute-$DEST.csv
-kubectl delete -f micro-rpc-faastroute.yaml
+hey -n 10000 -c 1 -t 30 -o csv http://10.43.190.1 >> micro-rpc-single-mmap-$DEST.csv
+kubectl delete -f micro-rpc-single-mmap.yaml
 sleep 1000s
 sed -i "s/$SIZE/1000000/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a/func.yaml
 sed -i "s/$SIZE/1000000/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-b/func.yaml
 sed -i "s/$SIZE/1000000/g" ../serverless-workflow-topology-survey/benchmarks/micro/micro-rpc-a-b/func.yaml
-sed -i "s/$SIZE/1000000/g" micro-rpc-grpc.yaml
-sed -i "s/$SIZE/1000000/g" micro-rpc-pod.yaml
-sed -i "s/$SIZE/1000000/g" micro-rpc-faastroute.yaml
+sed -i "s/$SIZE/1000000/g" micro-rpc-multi-pod.yaml
+sed -i "s/$SIZE/1000000/g" micro-rpc-single-pod.yaml
+sed -i "s/$SIZE/1000000/g" micro-rpc-single-mmap.yaml
--- micro-collection/micro-temp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./micro-invoke-temp.sh 50MB 50000000
-./micro-invoke-temp.sh 100MB 100000000
