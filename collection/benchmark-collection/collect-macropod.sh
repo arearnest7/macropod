200f56fcd6457df1f69278dccc3267c8297dbc7b
arearnest7 <arearnest7@localhost.localdomain>
2024-06-02 22:31:33 -0500

    fixed typos in collection scripts from moving folder (whoops)

--- collection/benchmark-collection/collect-macropod.sh
+++ collection/benchmark-collection/collect-macropod.sh
@@ -3,11 +3,9 @@ BENCHMARK=${1:-election}
 C=${2:-1}
 PAYLOAD=${3:-../payloads/election.json}
 N=${4:-10000}
-cd ../../tools/deployment-yamls
 curl -d @../../workflow-definitions/$BENCHMARK.json -X POST -H "Content-Type: application/json" http://10.43.190.1/create/$BENCHMARK
 sleep 180s
-hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1/$BENCHMARK >> ../../collection/benchmark-collection/macropod-$BENCHMARK.csv
-curl http://10.43.190.1/logs/$BENCHMARK >> ../../collection/benchmark-collection/macropod-$BENCHMARK-log-bundle.csv
+hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1/$BENCHMARK >> macropod-$BENCHMARK.csv
+curl http://10.43.190.1/logs/$BENCHMARK >> macropod-$BENCHMARK-log-bundle.csv
 curl http://10.43.190.1/delete/$BENCHMARK
 sleep 180s
-cd ../../collection/benchmark-collection/
--- collection/benchmark-collection/collect-yaml-fixed.sh
+++ collection/benchmark-collection/collect-yaml-fixed.sh
@@ -4,40 +4,40 @@ C=${2:-1}
 PAYLOAD=${3:-../payloads/election.json}
 N=${4:-10000}
 containers=(${5:-"election-gateway election-get-results election-vote-enqueuer election-vote-processor"})
-cd ../../tools/deployment-yamls
-kubectl apply -f ./$BENCHMARK-multi-oci.yaml
+cd ../../tools/workflow
+kubectl apply -f ./fixed-oci/$BENCHMARK-multi-oci.yaml
 sleep 180s
 hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1 >> ../../collection/benchmark-collection/multi-oci-$BENCHMARK.csv
 logs=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
 for i in $logs; do kubectl logs $i >> ../../collection/benchmark-collection/multi-oci-$i.csv; done;
-kubectl delete -f ./$BENCHMARK-multi-oci.yaml
+kubectl delete -f ./fixed-oci/$BENCHMARK-multi-oci.yaml
 sleep 180s
-kubectl apply -f ./$BENCHMARK-multi-pod.yaml
+kubectl apply -f ./fixed-macropod/$BENCHMARK-multi-pod.yaml
 sleep 180s
 hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1 >> ../../collection/benchmark-collection/multi-pod-$BENCHMARK.csv
 logs=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
 for i in $logs; do kubectl logs $i >> ../../collection/benchmark-collection/multi-pod-$i.csv; done;
-kubectl delete -f ./$BENCHMARK-multi-pod.yaml
+kubectl delete -f ./fixed-macropod/$BENCHMARK-multi-pod.yaml
 sleep 180s
-kubectl apply -f ./$BENCHMARK-single-mmap.yaml
+kubectl apply -f ./fixed-macropod/$BENCHMARK-single-mmap.yaml
 sleep 180s
 hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1 >> ../../collection/benchmark-collection/single-mmap-$BENCHMARK.csv
 logs=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
 for i in $logs; do for container in ${containers[@]}; do kubectl logs $i -c $container >> ../../collection/benchmark-collection/single-mmap-$container-$i.csv; done; done;
-kubectl delete -f ./$BENCHMARK-single-mmap.yaml
+kubectl delete -f ./fixed-macropod/$BENCHMARK-single-mmap.yaml
 sleep 180s
-kubectl apply -f ./$BENCHMARK-single-oci.yaml
+kubectl apply -f ./fixed-oci/$BENCHMARK-single-oci.yaml
 sleep 180s
 hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1 >> ../../collection/benchmark-collection/single-oci-$BENCHMARK.csv
 logs=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
 for i in $logs; do for container in ${containers[@]}; do kubectl logs $i -c $container >> ../../collection/benchmark-collection/single-oci-$container-$i.csv; done; done;
-kubectl delete -f ./$BENCHMARK-single-oci.yaml
+kubectl delete -f ./fixed-oci/$BENCHMARK-single-oci.yaml
 sleep 180s
-kubectl apply -f ./$BENCHMARK-single-pod.yaml
+kubectl apply -f ./fixed-macropod/$BENCHMARK-single-pod.yaml
 sleep 180s
 hey -n $N -c $C -t 180 -o csv -D $PAYLOAD -m POST -T application/json http://10.43.190.1 >> ../../collection/benchmark-collection/single-pod-$BENCHMARK.csv
 logs=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
 for i in $logs; do for container in ${containers[@]}; do kubectl logs $i -c $container >> ../../collection/benchmark-collection/single-pod-$container-$i.csv; done; done;
-kubectl delete -f ./$BENCHMARK-single-pod.yaml
+kubectl delete -f ./fixed-macropod/$BENCHMARK-single-pod.yaml
 cd ../../collection/benchmark-collection/
 sleep 180s
