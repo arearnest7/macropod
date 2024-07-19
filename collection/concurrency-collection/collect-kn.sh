ba91637f0748b008fb6bbe8c73c305760fa7cd06
arearnest7 <arearnest7@localhost.localdomain>
2024-07-18 19:43:05 -0500

    fixed namespace references in log collections for knative

--- collection/concurrency-collection/collect-kn.sh
+++ collection/concurrency-collection/collect-kn.sh
@@ -10,8 +10,8 @@ cd ../../tools/workflow/kn/
 sleep 180s
 hey -n $C -c $C -t 1000 -o csv -D $PAYLOAD -m POST -T application/json http://$ENTRY.knative-functions.$HOST.sslip.io >> ../../../collection/concurrency-collection/cold-start
 hey -n $N -c $C -t 1000 -o csv -D $PAYLOAD -m POST -T application/json http://$ENTRY.knative-functions.$HOST.sslip.io >> ../../../collection/concurrency-collection/kn-$SCRIPT.csv
-logs=$(sudo kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
-for i in $logs; do sudo kubectl logs -c user-container $i >> ../../../collection/concurrency-collection/kn-$SCRIPT-$i.csv; done;
+logs=$(sudo kubectl get pods -n knative-functions --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
+for i in $logs; do sudo kubectl logs -n knative-functions -c user-container $i >> ../../../collection/concurrency-collection/kn-$SCRIPT-$i.csv; done;
 ../remove-kn/remove-$SCRIPT.sh
 cd ../../../collection/concurrency-collection/
 sleep 180s
--- collection/fixed-collection/collect-kn.sh
+++ collection/fixed-collection/collect-kn.sh
@@ -9,8 +9,8 @@ cd ../../tools/workflow/kn/
 ./deploy-$SCRIPT.sh
 sleep 180s
 hey -n $N -c $C -t 1000 -o csv -D $PAYLOAD -m POST -T application/json http://$ENTRY.knative-functions.$HOST.sslip.io >> ../../../collection/fixed-collection/kn-$SCRIPT.csv
-logs=$(sudo kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
-for i in $logs; do sudo kubectl logs -c user-container $i >> ../../../collection/fixed-collection/kn-$SCRIPT-$i.csv; done;
+logs=$(sudo kubectl get pods -n knative-functions --no-headers -o custom-columns=":metadata.name" --sort-by="metadata.name")
+for i in $logs; do sudo kubectl logs -n knative-functions -c user-container $i >> ../../../collection/fixed-collection/kn-$SCRIPT-$i.csv; done;
 ../remove-kn/remove-$SCRIPT.sh
 cd ../../../collection/fixed-collection/
 sleep 180s
