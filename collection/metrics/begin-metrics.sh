9bca70272ea788cd59e670e9fcad15ee73d76eae
arearnest7 <arearnest7@sys-dev-vm-1.engr.tamu.edu>
2024-07-23 14:55:27 -0500

    finalized metric collection scripts to be background tasks and to kill all related metric processes at destination

--- collection/metrics/begin-metrics.sh
+++ collection/metrics/begin-metrics.sh
@@ -2,4 +2,4 @@
 user=$1
 interface=$2
 metric_nodes=($3)
-for i in ${metric_nodes[@]}; do ssh $user@$i "cd /home/$user/metrics && nohup go run metrics.go $interface metrics.csv &"; done;
+for i in ${metric_nodes[@]}; do ssh $user@$i "cd /home/$user/metrics; nohup env PATH=$PATH:/usr/local/go/bin go run metrics.go $interface metrics.csv 1>/dev/null 2>/dev/null &"; done;
