07e160f3cacf8d507c3eda9f1ddc086fa0943a8f
arearnest7 <arearnest7@sys-dev-vm-1.engr.tamu.edu>
2024-07-23 20:22:21 -0500

    changing cpu to be total-idle, while also adding all other CPU values independently

--- tools/collection/metrics/metrics.go
+++ tools/collection/metrics/metrics.go
@@ -17,7 +17,7 @@ func main() {
     } else {
         o, _ := os.Create(os.Args[2])
         writer := csv.NewWriter(o)
-        headers := []string{"timestamp", "cpu_total", "used_memory", "bytes_sent"}
+        headers := []string{"timestamp", "cpu_total_wo_idle", "used_memory", "bytes_sent", "cpu_user", "cpu_nice", "cpu_system", "cpu_idle", "cpu_iowait", "cpu_irq", "cpu_softirq", "cpu_steal", "cpu_guest", "cpu_guestnice", "cpu_total"}
         writer.Write(headers)
         normalization_set := false
         var bytes_sent_norm float64
@@ -38,9 +38,20 @@ func main() {
             }
             c := make([]string, 0)
             c = append(c, time.Now().UTC().Format("2006-01-02 15:04:05 UTC"))
-            c = append(c, strconv.FormatFloat(float64(cpu.User), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Total-cpu.Idle), 'f', -1, 64))
             c = append(c, strconv.FormatFloat(float64(mem.Used), 'f', -1, 64))
             c = append(c, strconv.FormatFloat(float64(net.TxBytes) - bytes_sent_norm, 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.User), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Nice), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.System), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Idle), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Iowait), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Irq), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Softirq), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Steal), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Guest), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.GuestNice), 'f', -1, 64))
+            c = append(c, strconv.FormatFloat(float64(cpu.Total), 'f', -1, 64))
             writer.Write(c)
             writer.Flush()
             bytes_sent_norm = float64(net.TxBytes)
