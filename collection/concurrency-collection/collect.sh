ee5fcb634cca76c1a816a6cd0740c1f5e76c07bb
arearnest7 <arearnest7@localhost.localdomain>
2024-07-24 22:59:08 -0500

    changed fixed collection to have less configurable values (fixed values) to simplify; simplified concurrency collection as well while also adding more collections (in steps of 10) and finish collection at concurrency 250 (except for some like video)

--- collection/concurrency-collection/collect.sh
+++ collection/concurrency-collection/collect.sh
@@ -1,71 +1,119 @@
 #!/bin/bash
 HOST=${1:-127.0.0.1}
+DIR=${2:-results}
 ./collect-election.sh $HOST 1 500 >> election-1.out
 ./collect-election.sh $HOST 2 500 >> election-2.out
 ./collect-election.sh $HOST 4 500 >> election-4.out
 ./collect-election.sh $HOST 8 500 >> election-8.out
 ./collect-election.sh $HOST 16 500 >> election-16.out
-./collect-election.sh $HOST 32 500 >> election-32.out
-./collect-election.sh $HOST 64 500 >> election-64.out
-./collect-election.sh $HOST 128 500 >> election-128.out
-./collect-election.sh $HOST 256 500 >> election-256.out
+./collect-election.sh $HOST 20 500 >> election-20.out
+./collect-election.sh $HOST 30 500 >> election-30.out
+./collect-election.sh $HOST 40 500 >> election-40.out
+./collect-election.sh $HOST 50 500 >> election-50.out
+./collect-election.sh $HOST 60 500 >> election-60.out
+./collect-election.sh $HOST 70 500 >> election-70.out
+./collect-election.sh $HOST 80 500 >> election-80.out
+./collect-election.sh $HOST 90 500 >> election-90.out
+./collect-election.sh $HOST 100 500 >> election-100.out
+./collect-election.sh $HOST 110 500 >> election-110.out
+./collect-election.sh $HOST 120 500 >> election-120.out
+./collect-election.sh $HOST 130 500 >> election-130.out
+./collect-election.sh $HOST 140 500 >> election-140.out
+./collect-election.sh $HOST 150 500 >> election-150.out
+./collect-election.sh $HOST 160 500 >> election-160.out
+./collect-election.sh $HOST 170 500 >> election-170.out
+./collect-election.sh $HOST 180 500 >> election-180.out
+./collect-election.sh $HOST 190 500 >> election-190.out
+./collect-election.sh $HOST 200 500 >> election-200.out
+./collect-election.sh $HOST 210 500 >> election-210.out
+./collect-election.sh $HOST 220 500 >> election-220.out
+./collect-election.sh $HOST 230 500 >> election-230.out
+./collect-election.sh $HOST 240 500 >> election-240.out
+./collect-election.sh $HOST 250 500 >> election-250.out
 
 #./collect-feature.sh $HOST 1 500 >> feature-1.out
 #./collect-feature.sh $HOST 2 500 >> feature-2.out
 #./collect-feature.sh $HOST 4 500 >> feature-4.out
 #./collect-feature.sh $HOST 8 500 >> feature-8.out
-#./collect-feature.sh $HOST 16 500 >> feature-16.out
-#./collect-feature.sh $HOST 32 500 >> feature-32.out
-#./collect-feature.sh $HOST 64 500 >> feature-64.out
-#./collect-feature.sh $HOST 128 500 >> feature-128.out
-#./collect-feature.sh $HOST 256 500 >> feature-256.out
 
 ./collect-hotel.sh $HOST 1 500 >> hotel-1.out
 ./collect-hotel.sh $HOST 2 500 >> hotel-2.out
 ./collect-hotel.sh $HOST 4 500 >> hotel-4.out
 ./collect-hotel.sh $HOST 8 500 >> hotel-8.out
 ./collect-hotel.sh $HOST 16 500 >> hotel-16.out
-./collect-hotel.sh $HOST 32 500 >> hotel-32.out
-./collect-hotel.sh $HOST 64 500 >> hotel-64.out
-./collect-hotel.sh $HOST 128 500 >> hotel-128.out
-./collect-hotel.sh $HOST 256 500 >> hotel-256.out
+./collect-hotel.sh $HOST 20 500 >> hotel-20.out
+./collect-hotel.sh $HOST 30 500 >> hotel-30.out
+./collect-hotel.sh $HOST 40 500 >> hotel-40.out
+./collect-hotel.sh $HOST 50 500 >> hotel-50.out
+./collect-hotel.sh $HOST 60 500 >> hotel-60.out
+./collect-hotel.sh $HOST 70 500 >> hotel-70.out
+./collect-hotel.sh $HOST 80 500 >> hotel-80.out
+./collect-hotel.sh $HOST 90 500 >> hotel-90.out
+./collect-hotel.sh $HOST 100 500 >> hotel-100.out
+./collect-hotel.sh $HOST 110 500 >> hotel-110.out
+./collect-hotel.sh $HOST 120 500 >> hotel-120.out
+./collect-hotel.sh $HOST 130 500 >> hotel-130.out
+./collect-hotel.sh $HOST 140 500 >> hotel-140.out
+./collect-hotel.sh $HOST 150 500 >> hotel-150.out
+./collect-hotel.sh $HOST 160 500 >> hotel-160.out
+./collect-hotel.sh $HOST 170 500 >> hotel-170.out
+./collect-hotel.sh $HOST 180 500 >> hotel-180.out
+./collect-hotel.sh $HOST 190 500 >> hotel-190.out
+./collect-hotel.sh $HOST 200 500 >> hotel-200.out
+./collect-hotel.sh $HOST 210 500 >> hotel-210.out
+./collect-hotel.sh $HOST 220 500 >> hotel-220.out
+./collect-hotel.sh $HOST 230 500 >> hotel-230.out
+./collect-hotel.sh $HOST 240 500 >> hotel-240.out
+./collect-hotel.sh $HOST 250 500 >> hotel-250.out
 
 #./collect-pipelined.sh $HOST 1 500 >> pipelined-1.out
 #./collect-pipelined.sh $HOST 2 500 >> pipelined-2.out
 #./collect-pipelined.sh $HOST 4 500 >> pipelined-4.out
 #./collect-pipelined.sh $HOST 8 500 >> pipelined-8.out
 #./collect-pipelined.sh $HOST 16 500 >> pipelined-16.out
-#./collect-pipelined.sh $HOST 32 500 >> pipelined-32.out
-#./collect-pipelined.sh $HOST 64 500 >> pipelined-64.out
-#./collect-pipelined.sh $HOST 128 500 >> pipelined-128.out
-#./collect-pipelined.sh $HOST 256 500 >> pipelined-256.out
 
 ./collect-sentiment.sh $HOST 1 500 >> sentiment-1.out
 ./collect-sentiment.sh $HOST 2 500 >> sentiment-2.out
 ./collect-sentiment.sh $HOST 4 500 >> sentiment-4.out
 ./collect-sentiment.sh $HOST 8 500 >> sentiment-8.out
 ./collect-sentiment.sh $HOST 16 500 >> sentiment-16.out
-./collect-sentiment.sh $HOST 32 500 >> sentiment-32.out
-./collect-sentiment.sh $HOST 64 500 >> sentiment-64.out
-./collect-sentiment.sh $HOST 128 500 >> sentiment-128.out
-./collect-sentiment.sh $HOST 256 500 >> sentiment-256.out
+./collect-sentiment.sh $HOST 20 500 >> sentiment-20.out
+./collect-sentiment.sh $HOST 30 500 >> sentiment-30.out
+./collect-sentiment.sh $HOST 40 500 >> sentiment-40.out
+./collect-sentiment.sh $HOST 50 500 >> sentiment-50.out
+./collect-sentiment.sh $HOST 60 500 >> sentiment-60.out
+./collect-sentiment.sh $HOST 70 500 >> sentiment-70.out
+./collect-sentiment.sh $HOST 80 500 >> sentiment-80.out
+./collect-sentiment.sh $HOST 90 500 >> sentiment-90.out
+./collect-sentiment.sh $HOST 100 500 >> sentiment-100.out
+./collect-sentiment.sh $HOST 110 500 >> sentiment-110.out
+./collect-sentiment.sh $HOST 120 500 >> sentiment-120.out
+./collect-sentiment.sh $HOST 130 500 >> sentiment-130.out
+./collect-sentiment.sh $HOST 140 500 >> sentiment-140.out
+./collect-sentiment.sh $HOST 150 500 >> sentiment-150.out
+./collect-sentiment.sh $HOST 160 500 >> sentiment-160.out
+./collect-sentiment.sh $HOST 170 500 >> sentiment-170.out
+./collect-sentiment.sh $HOST 180 500 >> sentiment-180.out
+./collect-sentiment.sh $HOST 190 500 >> sentiment-190.out
+./collect-sentiment.sh $HOST 200 500 >> sentiment-200.out
+./collect-sentiment.sh $HOST 210 500 >> sentiment-210.out
+./collect-sentiment.sh $HOST 220 500 >> sentiment-220.out
+./collect-sentiment.sh $HOST 230 500 >> sentiment-230.out
+./collect-sentiment.sh $HOST 240 500 >> sentiment-240.out
+./collect-sentiment.sh $HOST 250 500 >> sentiment-250.out
 
 ./collect-video.sh $HOST 1 500 >> video-1.out
 ./collect-video.sh $HOST 2 500 >> video-2.out
 ./collect-video.sh $HOST 4 500 >> video-4.out
 ./collect-video.sh $HOST 8 500 >> video-8.out
-./collect-video.sh $HOST 16 500 >> video-16.out
-./collect-video.sh $HOST 32 500 >> video-32.out
-./collect-video.sh $HOST 64 500 >> video-64.out
-./collect-video.sh $HOST 128 500 >> video-128.out
-./collect-video.sh $HOST 256 500 >> video-256.out
 
 #./collect-wage.sh $HOST 1 500 >> wage-1.out
 #./collect-wage.sh $HOST 2 500 >> wage-2.out
 #./collect-wage.sh $HOST 4 500 >> wage-4.out
 #./collect-wage.sh $HOST 8 500 >> wage-8.out
 #./collect-wage.sh $HOST 16 500 >> wage-16.out
-#./collect-wage.sh $HOST 32 500 >> wage-32.out
-#./collect-wage.sh $HOST 64 500 >> wage-64.out
-#./collect-wage.sh $HOST 128 500 >> wage-128.out
-#./collect-wage.sh $HOST 256 500 >> wage-256.out
+
+mkdir $DIR
+mv *.csv $DIR
+mv *.out $DIR
+mv cold-start $DIR
--- collection/fixed-collection/collect.sh
+++ collection/fixed-collection/collect.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 HOST=${1:-127.0.0.1}
-C=${2:-1}
-N=${3:-10000}
-DIR=${4:-results}
-./collect-kn.sh $HOST full-election election-full $C ../../payloads/election.json $N
-./collect-kn.sh $HOST full-feature feature-full $C ../../payloads/feature.json $N
-./collect-kn.sh $HOST full-hotel hotel-full $C ../../payloads/hotel.json $N
-./collect-kn.sh $HOST full-pipelined pipelined-full $C ../../payloads/pipelined.json $N
-./collect-kn.sh $HOST full-sentiment sentiment-full $C ../../payloads/sentiment.json $N
-./collect-kn.sh $HOST full-video video-full $C ../../payloads/video.json $N
-./collect-kn.sh $HOST full-wage wage-full $C ../../payloads/wage.json $N
-./collect-kn.sh $HOST original-election election-gateway $C ../../payloads/election.json $N
-./collect-kn.sh $HOST original-feature feature-orchestrator $C ../../payloads/feature.json $N
-./collect-kn.sh $HOST original-hotel hotel-frontend $C ../../payloads/hotel.json $N
-./collect-kn.sh $HOST original-pipelined pipelined-main $C ../../payloads/pipelined.json $N
-./collect-kn.sh $HOST original-sentiment sentiment-main $C ../../payloads/sentiment.json $N
-./collect-kn.sh $HOST original-video video-streaming $C ../../payloads/video.json $N
-./collect-kn.sh $HOST original-wage wage-validator $C ../../payloads/wage.json $N
-./collect-kn.sh $HOST partial-election election-gateway-vevp $C ../../payloads/election.json $N
-./collect-kn.sh $HOST partial-feature feature-orchestrator-wsr $C ../../payloads/feature.json $N
-./collect-kn.sh $HOST partial-hotel hotel-frontend-spgr $C ../../payloads/hotel.json $N
-./collect-kn.sh $HOST partial-pipelined pipelined-main-partial $C ../../payloads/pipelined.json $N
-./collect-kn.sh $HOST partial-sentiment sentiment-main-rcposc $C ../../payloads/sentiment.json $N
-./collect-kn.sh $HOST partial-video video-streaming-d $C ../../payloads/video.json $N
-./collect-kn.sh $HOST partial-wage wage-validator-fw $C ../../payloads/wage.json $N
-./collect-yaml.sh election $C ../payloads/election.json $N "election-gateway election-get-results election-vote-enqueuer election-vote-processor"
-./collect-yaml.sh feature $C ../payloads/feature.json $N "feature-orchestrator feature-extractor feature-wait feature-status feature-reducer"
-./collect-yaml.sh hotel $C ../payloads/hotel.json $N "hotel-frontend hotel-search hotel-recommend hotel-reserve hotel-user hotel-geo hotel-profile hotel-rate"
-./collect-yaml.sh pipelined $C ../payloads/pipelined.json $N "pipelined-main pipelined-main-2 pipelined-main-3 pipelined-checksum pipelined-zip pipelined-encrypt"
-./collect-yaml.sh sentiment $C ../payloads/sentiment.json $N "sentiment-cfail sentiment-db sentiment-main sentiment-product-or-service sentiment-product-result sentiment-product-sentiment sentiment-read-csv sentiment-service-result sentiment-service-sentiment sentiment-sfail sentiment-sns"
-./collect-yaml.sh video $C ../payloads/video.json $N "video-streaming video-decoder video-recog"
-./collect-yaml.sh wage $C ../payloads/wage.json $N "wage-validator wage-format wage-write-raw wage-stats wage-sum wage-avg wage-merit wage-write-merit"
+DIR=${2:-results}
+./collect-kn.sh $HOST full-election election-full 1 ../../payloads/election.json 500
+./collect-kn.sh $HOST full-feature feature-full 1 ../../payloads/feature.json 500
+./collect-kn.sh $HOST full-hotel hotel-full 1 ../../payloads/hotel.json 500
+./collect-kn.sh $HOST full-pipelined pipelined-full 1 ../../payloads/pipelined.json 500
+./collect-kn.sh $HOST full-sentiment sentiment-full 1 ../../payloads/sentiment.json 500
+./collect-kn.sh $HOST full-video video-full 1 ../../payloads/video.json 500
+./collect-kn.sh $HOST full-wage wage-full 1 ../../payloads/wage.json 500
+./collect-kn.sh $HOST original-election election-gateway 1 ../../payloads/election.json 500
+./collect-kn.sh $HOST original-feature feature-orchestrator 1 ../../payloads/feature.json 500
+./collect-kn.sh $HOST original-hotel hotel-frontend 1 ../../payloads/hotel.json 500
+./collect-kn.sh $HOST original-pipelined pipelined-main 1 ../../payloads/pipelined.json 500
+./collect-kn.sh $HOST original-sentiment sentiment-main 1 ../../payloads/sentiment.json 500
+./collect-kn.sh $HOST original-video video-streaming 1 ../../payloads/video.json 500
+./collect-kn.sh $HOST original-wage wage-validator 1 ../../payloads/wage.json 500
+./collect-kn.sh $HOST partial-election election-gateway-vevp 1 ../../payloads/election.json 500
+./collect-kn.sh $HOST partial-feature feature-orchestrator-wsr 1 ../../payloads/feature.json 500
+./collect-kn.sh $HOST partial-hotel hotel-frontend-spgr 1 ../../payloads/hotel.json 500
+./collect-kn.sh $HOST partial-pipelined pipelined-main-partial 1 ../../payloads/pipelined.json 500
+./collect-kn.sh $HOST partial-sentiment sentiment-main-rcposc 1 ../../payloads/sentiment.json 500
+./collect-kn.sh $HOST partial-video video-streaming-d 1 ../../payloads/video.json 500
+./collect-kn.sh $HOST partial-wage wage-validator-fw 1 ../../payloads/wage.json 500
+./collect-yaml.sh election 1 ../payloads/election.json 500 "election-gateway election-get-results election-vote-enqueuer election-vote-processor"
+./collect-yaml.sh feature 1 ../payloads/feature.json 500 "feature-orchestrator feature-extractor feature-wait feature-status feature-reducer"
+./collect-yaml.sh hotel 1 ../payloads/hotel.json 500 "hotel-frontend hotel-search hotel-recommend hotel-reserve hotel-user hotel-geo hotel-profile hotel-rate"
+./collect-yaml.sh pipelined 1 ../payloads/pipelined.json 500 "pipelined-main pipelined-main-2 pipelined-main-3 pipelined-checksum pipelined-zip pipelined-encrypt"
+./collect-yaml.sh sentiment 1 ../payloads/sentiment.json 500 "sentiment-cfail sentiment-db sentiment-main sentiment-product-or-service sentiment-product-result sentiment-product-sentiment sentiment-read-csv sentiment-service-result sentiment-service-sentiment sentiment-sfail sentiment-sns"
+./collect-yaml.sh video 1 ../payloads/video.json 500 "video-streaming video-decoder video-recog"
+./collect-yaml.sh wage 1 ../payloads/wage.json 500 "wage-validator wage-format wage-write-raw wage-stats wage-sum wage-avg wage-merit wage-write-merit"
 mkdir $DIR
 mv *.csv $DIR
