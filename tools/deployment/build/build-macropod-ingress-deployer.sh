324f539acb9807a6f843950767e81397f7751972
sysdev-tamu <sysdev@sys-dev-vm-1.engr.tamu.edu>
2024-12-06 12:44:49 -0600

    macropod-http branch setup

--- tools/deployment/build/build-macropod-ingress-deployer.sh
+++ tools/deployment/build/build-macropod-ingress-deployer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ID=${1:-sysdevtamu}
-sudo DOCKER_BUILDKIT=1 docker build ../../../macropod-base/macropod-ingress -t $ID/macropod-ingress:latest && sudo docker push $ID/macropod-ingress:latest
-sudo DOCKER_BUILDKIT=1 docker build ../../../macropod-base/macropod-deployer -t $ID/macropod-deployer:latest && sudo docker push $ID/macropod-deployer:latest
+sudo DOCKER_BUILDKIT=1 docker build ../../../macropod-base/macropod-ingress -t $ID/macropod-ingress:http && sudo docker push $ID/macropod-ingress:http
+sudo DOCKER_BUILDKIT=1 docker build ../../../macropod-base/macropod-deployer -t $ID/macropod-deployer:http && sudo docker push $ID/macropod-deployer:http
