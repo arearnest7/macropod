9bdee1a7fb633cdb2a5bdc1cd07a3bae01f80608
sysdev-tamu <sysdev@sys-dev-vm-1.engr.tamu.edu>
2025-06-04 17:29:39 -0500

    fixed incorrect /dev/null references

--- collection/kn-scripts/deploy-election-unified.sh
+++ collection/kn-scripts/deploy-election-unified.sh
@@ -2,6 +2,6 @@
 host=${1:-127.0.0.1}
 for i in yamls/election*; do sed -i "s/knative-functions\.127\.0\.0\.1/knative-functions\.$host/g" $i; done;
 
-kubectl apply -f yamls/election-unified.yaml 2>dev/null
+kubectl apply -f yamls/election-unified.yaml 2>/dev/null
 
 for i in yamls/election*; do sed -i "s/knative-functions\.$host/knative-functions\.127\.0\.0\.1/g" $i; done;
--- collection/kn-scripts/deploy-wage-original.sh
+++ collection/kn-scripts/deploy-wage-original.sh
@@ -2,13 +2,13 @@
 host=${1:-127.0.0.1}
 for i in yamls/wage*; do sed -i "s/knative-functions\.127\.0\.0\.1/knative-functions\.$host/g" $i; done;
 
-kubectl apply -f yamls/wage-avg.yaml 2>dev/null
-kubectl apply -f yamls/wage-format.yaml 2>dev/null
-kubectl apply -f yamls/wage-merit.yaml 2>dev/null
-kubectl apply -f yamls/wage-stats.yaml 2>dev/null
-kubectl apply -f yamls/wage-sum.yaml 2>dev/null
-kubectl apply -f yamls/wage-validator.yaml 2>dev/null
-kubectl apply -f yamls/wage-write-merit.yaml 2>dev/null
-kubectl apply -f yamls/wage-write-raw.yaml 2>dev/null
+kubectl apply -f yamls/wage-avg.yaml 2>/dev/null
+kubectl apply -f yamls/wage-format.yaml 2>/dev/null
+kubectl apply -f yamls/wage-merit.yaml 2>/dev/null
+kubectl apply -f yamls/wage-stats.yaml 2>/dev/null
+kubectl apply -f yamls/wage-sum.yaml 2>/dev/null
+kubectl apply -f yamls/wage-validator.yaml 2>/dev/null
+kubectl apply -f yamls/wage-write-merit.yaml 2>/dev/null
+kubectl apply -f yamls/wage-write-raw.yaml 2>/dev/null
 
 for i in yamls/wage*; do sed -i "s/knative-functions\.$host/knative-functions\.127\.0\.0\.1/g" $i; done;
