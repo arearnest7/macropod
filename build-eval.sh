6b3a28b01e98341e90fb2218c9fe5344d5e59a3d
sysdev-tamu <sysdev@sys-dev-vm-1.engr.tamu.edu>
2025-07-07 18:36:01 -0500

    removing temp files

--- build-eval.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ID=${1:-sysdevtamu}
-TAG=${2:-latest}
-sudo docker buildx build base/macropod-eval -t $ID/macropod-eval:$TAG && sudo docker push $ID/macropod-eval:$TAG
-
--- build-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ID=${1:-sysdevtamu}
-TAG=${2:-latest}
-sudo docker buildx build base/macropod-ingress -t $ID/macropod-ingress:$TAG && sudo docker push $ID/macropod-ingress:$TAG
-sudo docker buildx build base/macropod-deployer -t $ID/macropod-deployer:$TAG && sudo docker push $ID/macropod-deployer:$TAG
-
